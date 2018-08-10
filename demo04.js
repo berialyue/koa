4ddafb49cc7c647297f4a786378bf46462387ab6
berialyue <berialyue@gmail.com>
2018-08-10 16:41:03 +0800

    add koa-bodyParser

---
+++ demo04.js
@@ -0,0 +1,32 @@
+const Koa = require('Koa')
+const bodyParser = require('koa-bodyparser')
+const app = new Koa()
+
+app.use(bodyParser())
+
+app.use(async(ctx)=>{
+  if(ctx.url === '/' && ctx.method === 'GET'){
+    let html = `
+      <h1>Koa2 request post demo</h1>
+      <form method='POST' action='/'>
+        <p>userName</p>
+        <input name="userName" /> <br/>
+        <p>age</p>
+        <input name="age" /> <br/>
+        <p>webSite</p>
+        <input name='webSite' /><br/>
+        <button type="submit">submit</button>
+      </form>
+    `
+    ctx.body = html
+  } else if(ctx.url === '/' && ctx.method === 'POST'){
+    let postData = ctx.request.body
+    ctx.body = postData
+  }else {
+    ctx.body = '<h1>404</h1>'
+  }
+})
+
+app.listen(3000,()=>{
+  console.log('[demo] server is starting at port 3000')
+})
